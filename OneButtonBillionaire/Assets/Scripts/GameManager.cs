67542eb3e8a1c96b907b257f802db80c848e2e68
Jonathan <>
2025-09-20 17:06:46 +0200

    add images and gameloop code

--- OneButtonBillionaire/Assets/Scripts/GameManager.cs
+++ OneButtonBillionaire/Assets/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -12,10 +13,78 @@ public class GameManager : MonoBehaviour
         }
     }
 
+
+    private bool _inputValid = false;
+    private int _score = 0;
+
+    private HashSet<string> used = new System.Collections.Generic.HashSet<string>();
+    private string entryA = "";
+    private string entryB = "";
+    private string entryC = "";
+
+
+
+
+
+    private void setEntries()
+    {
+        used.Clear();
+        entryA = MorseAlphabet.GetRandomMorseLetter();
+        used.Add(entryA);
+
+        do {
+            entryB = MorseAlphabet.GetRandomMorseLetter();
+        } while (used.Contains(entryB));
+        used.Add(entryB);
+
+        do {
+            entryC = MorseAlphabet.GetRandomMorseLetter();
+        } while (used.Contains(entryC));
+        used.Add(entryC);
+    }
+
+    private void clearEntries()
+    {
+        used.Clear();
+        entryA = "";
+        entryB = "";
+        entryC = "";
+    }
+
     private void Start()
     {
-        var entry = MorseAlphabet.GetRandomMorseLetter();
-        
-        Debug.Log(entry);
+        setEntries();
+        Debug.Log($"GameManager started: correct={entryA}, B={entryB}, C={entryC}");
+    }
+
+    private float timer = 0f;
+    private float interval = 5f;
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= interval)
+        {
+            // Place your timed logic here
+            if(_inputValid)
+            {
+                _score += 100;
+                //displayResults(true);
+                // Execute your logic for valid input
+            }
+            else
+            {
+                _score -= 100;
+                //displayResults(false);
+                // Execute your logic for invalid input
+            }
+
+
+
+            setEntries();
+            timer = 0f;
+
+            Debug.Log("5 seconds have passed!");
+        }
     }
 }
